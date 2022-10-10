77f22bde31317bec329b3142771db3fd52069fa8
Durgasree <shree1499@gmail.com>
2022-10-10 16:43:40 +0530

    sample crud api .net apicore

---
+++ Models/BookmyshowContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace bookmyshow_api.Models
+{
+    public class BookmyshowContext : DbContext
+    {
+        public BookmyshowContext(DbContextOptions<BookmyshowContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Theatre> Theatres { get; set; } = null!;
+    }
+}
